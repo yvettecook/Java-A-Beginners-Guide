2b20de370c480c3b9fcc1ee593815b6769d9d9cd
Yvette <yvettecook@gmail.com>
2015-01-21 10:35:49 +0000

    First java program

---
+++ Example.java
@@ -0,0 +1,11 @@
+/*
+This is a simple Java program
+
+*/
+
+class Example {
+  // A Java program begins with a call to main().
+  public static void main(String args[]) {
+    System.out.println("Java drives the web.");
+  }
+}
