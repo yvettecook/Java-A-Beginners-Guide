cbfb3d68ee018682007fbc67432cc2f411001a52
Yvette <yvettecook@gmail.com>
2015-01-21 13:09:37 +0000

    Types in Java

---
+++ Chapter2/VarIntDemo.java
@@ -0,0 +1,18 @@
+// Demonstrate the lifetime of a variable
+
+class VarIntDemo {
+
+  public static void main(String args[]) {
+
+    int x;
+
+    for(x = 0; x < 3; x++) {
+      int y = -1;
+      System.out.println("y is: " + y);
+      y = 100;
+      System.out.println("y is now: " + y);
+    }
+
+  }
+
+}
---
+++ ControlStatements/ForDemo.java
@@ -0,0 +1,16 @@
+// Demonstrating a for loop
+
+class ForDemo {
+
+  public static void main(String args[]) {
+
+    int count;
+
+    for(count = 0; count < 5; count = count + 1)
+      System.out.println("This is count: " + count);
+
+    System.out.println("Done!");
+
+  }
+
+}
