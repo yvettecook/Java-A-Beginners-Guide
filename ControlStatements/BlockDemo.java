6e596a0b0a2a607a9d5b676228ec992e1aa6f86e
Yvette <yvettecook@gmail.com>
2015-01-21 11:53:59 +0000

    Exploring for loops and if statements

---
+++ ControlStatements/BlockDemo.java
@@ -0,0 +1,20 @@
+// Demonstrating a block of code
+
+class BlockDemo {
+
+  public static void main(String args[]) {
+
+    double i, j, d;
+
+    i = 5;
+    j = 10;
+
+    if (i != 0) {
+      System.out.println("i does not equal 0");
+      d = j / i;
+      System.out.println("j / i is " + d);
+    }
+
+  }
+  
+}
