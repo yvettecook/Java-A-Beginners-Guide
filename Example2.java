232ce2220778676c02440a48c6508965243a668c
Yvette <yvettecook@gmail.com>
2015-01-21 10:50:41 +0000

    Second example - assigning variables

---
+++ Example2.java
@@ -0,0 +1,17 @@
+// This demonstrates a variable
+
+class Example2 {
+  public static void main(String args[]) {
+    int var1; // this declares a variable
+    int var2;
+
+    var1 = 1024;
+
+    System.out.println("var1 contains " + var1);
+
+    var2 = var1 / 2;
+
+    System.out.println(var2);
+
+  }
+}
